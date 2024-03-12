c3c4fc5674cc6a33bb4d9506958108d9d86afc61
goldangeee <goldangeee@proton.me>
2024-03-12 23:14:00 +0900

    ai begin

--- 1.py
+++ 1.py
@@ -1,3 +1,22 @@
 #pip install python-dotenv
 from dotenv import load_dotenv
+from openai import OpenAI
 import os
+
+# .env 파일에서 환경 변수를 로드합니다.
+load_dotenv()
+
+# 환경 변수를 사용하여 API 키를 불러옵니다.
+openai_api_key = os.getenv("OPENAI_API_KEY")
+
+client = OpenAI(api_key=openai_api_key)
+
+response = client.chat.completions.create(
+  model="gpt-3.5-turbo-0125",
+  response_format={ "type": "json_object" },
+  messages=[
+    {"role": "system", "content": "You are a helpful assistant designed to output JSON."},
+    {"role": "user", "content": "Who won the world series in 2020?"}
+  ]
+)
+print(response.choices[0].message.content)
